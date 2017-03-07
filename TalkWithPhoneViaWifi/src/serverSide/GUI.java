af6d5e87b760c5bf620a129e9229d02166c0f980
Noam Meerovitch <Noam Meerovitch>
2017-03-07 18:41:02 +0200

    Coded SwingWorker in order to seperate GUI from background work

--- TalkWithPhoneViaWifi/src/serverSide/GUI.java
+++ TalkWithPhoneViaWifi/src/serverSide/GUI.java
@@ -4,10 +4,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class GUI {
 
@@ -21,8 +24,8 @@ public class GUI {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Server server = new Server();
-					GUI window = new GUI(server);
+					//Server server = new Server();
+					GUI window = new GUI();
 					window.frmServerSide.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -34,16 +37,18 @@ public class GUI {
 	/**
 	 * Create the application.
 	 */
-	public GUI(Server server) {
+	public GUI() {
 		
-		initialize(server);
+		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize(Server server) {
+	private void initialize() {
+		
 		frmServerSide = new JFrame();
+		frmServerSide.setResizable(false);
 		frmServerSide.setTitle("Server Side - Listening To Phone");
 		frmServerSide.setBounds(100, 100, 450, 300);
 		frmServerSide.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,13 +58,35 @@ public class GUI {
 		lblPort.setBounds(10, 11, 37, 14);
 		frmServerSide.getContentPane().add(lblPort);
 		
-		JButton btnConnectAndSend = new JButton("Connect And Listen");
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 155, 414, 95);
+		frmServerSide.getContentPane().add(scrollPane);
+		
+		JTextArea taLog = new JTextArea();
+		scrollPane.setViewportView(taLog);
+		taLog.setWrapStyleWord(true);
+		taLog.setLineWrap(true);
+		
+		JButton btnConnectAndSend = new JButton("Listen");
+		
 		btnConnectAndSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Server server = new Server(btnConnectAndSend, taLog.getDocument());
+				try {
+					server.setPort(Integer.parseInt(tfPort.getText().toString())); 
 				
-				server.connectAndSend( Integer.parseInt(tfPort.getText().toString()));
+				} catch (NumberFormatException exception) {
+				
+					server.setPort(700);
+					System.out.println("No port provided, set to default: 700");
+					
+				}
+				btnConnectAndSend.setEnabled(false);
+				server.execute();
 			}
-		});
+			}
+		);
+		
 		btnConnectAndSend.setBounds(10, 36, 165, 23);
 		frmServerSide.getContentPane().add(btnConnectAndSend);
 		
@@ -67,5 +94,19 @@ public class GUI {
 		tfPort.setBounds(43, 8, 86, 20);
 		frmServerSide.getContentPane().add(tfPort);
 		tfPort.setColumns(10);
+		
+		JLabel lblIP = new JLabel("IP Address:");
+		lblIP.setBounds(10, 76, 67, 14);
+		frmServerSide.getContentPane().add(lblIP);
+		
+		/*
+		JLabel lbIPValue = new JLabel(server.getIP());
+		lbIPValue.setBounds(83, 76, 137, 14);
+		frmServerSide.getContentPane().add(lbIPValue);
+		*/
+		JLabel lblLog = new JLabel("Log:");
+		lblLog.setBounds(10, 140, 46, 14);
+		frmServerSide.getContentPane().add(lblLog);
+		
 	}
 }
--- TalkWithPhoneViaWifi/src/serverSide/Server.java
+++ TalkWithPhoneViaWifi/src/serverSide/Server.java
@@ -4,49 +4,113 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.SwingWorker;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
-public class Server {
+public class Server extends SwingWorker<Void, String> {
 
+	private int port;
+	//private JTextArea taLog;
+	private Document taDoc;
+	private JButton restartBtn;
 	
+	public Server(JButton btn, Document doc) {
+		restartBtn = btn;
+		taDoc = doc;
+		try {
+			taDoc.remove(0, taDoc.getLength());
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
-	public void connectAndSend(int port) {
-		ServerSocket server;
-		
+	public void setPort(int portFromUser) {
+		port = portFromUser;
+	}
+
+	public int getPort() {
+		return this.port;
+	}
+	
+	public String getIP() {
+		try {
+			String ia = InetAddress.getLocalHost().getHostAddress();
+			return ia;
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	@Override
+	protected Void doInBackground() throws Exception {
+		ServerSocket server = null;
 		try {
 			Robot robot = new Robot();
 			server = new ServerSocket(port);
 			port = server.getLocalPort();
 			System.out.println(port);
+			publish("Waiting for client to connect at port " + port + ":\n");
+			server.setSoTimeout(10000);
 			Socket socket = server.accept();
+			publish("Client connected!\n Bytes recived:");
 			InputStream is = null;
 			is = socket.getInputStream();
-			int data = -1;
-			int x = 0;
-			int y;
-			int newPos = 0;
-			while ((data = is.read()) != -1){
-				if (newPos == 0) {
-					x = data;
-					newPos++;
+			int x = 700;
+			int y = 500;
+			byte[] floatInByteArr = new byte[4];
+			int bytesRecived = 0; 
+			while ((is.read(floatInByteArr)) == 4) {	
+				float value = ByteBuffer.wrap(floatInByteArr).order(ByteOrder.BIG_ENDIAN).getFloat();
+				y += value;
+				robot.mouseMove(x, y);		
+				bytesRecived += 4;
+				publish(Integer.toString(bytesRecived));
 				}
-				if (newPos == 1) {
-					newPos = 0;
-					y = data;
-					//System.out.println(x);
-					robot.mouseMove(x, y);	
-				}
-				
-			}
 			server.close();
+		} catch (SocketTimeoutException e) {
+			if(null != server) server.close();
+			publish("Timeout: no client connected for 10 seconds");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} 
+		return null;
+	}
+
+	@Override
+	protected void process(List<String> chunks) {
+		// TODO Auto-generated method stub
+		super.process(chunks);
+		for(String publishedData : chunks) {
+			//Document doc = tpLog.getDocument();
+			try {
+				taDoc.insertString(taDoc.getLength(), publishedData + " " , null);
+			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-	}	
-}
+	}
+
+	@Override
+	protected void done() {
+		// TODO Auto-generated method stub
+		super.done();
+		restartBtn.setEnabled(true);
+	}
+}	
