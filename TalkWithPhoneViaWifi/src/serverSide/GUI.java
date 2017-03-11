3802d2e497317313eff39e55f7a29f27d35d1723
Noam Meerovitch <Noam Meerovitch>
2017-03-11 16:06:30 +0200

    Improved GUI, added the support of XY moving

--- TalkWithPhoneViaWifi/src/serverSide/GUI.java
+++ TalkWithPhoneViaWifi/src/serverSide/GUI.java
@@ -5,18 +5,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
-import javax.swing.JTextArea;
+import javax.swing.text.BadLocationException;
 
-public class GUI {
+import org.w3c.dom.css.RGBColor;
+
+import javax.swing.JTextArea;
+import javax.swing.JRadioButton;
+
+public class GUI implements ActionListener {
 
 	private JFrame frmServerSide;
 	private JTextField tfPort;
-
+	private JButton btnConnectAndSend;
+	private JTextArea taControlLog;
+	private JTextArea taDataLog;
+	private JRadioButton rbFloat;
+	private JRadioButton rbObject;
+	private ButtonGroup rg;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -50,48 +62,46 @@ public class GUI {
 		frmServerSide = new JFrame();
 		frmServerSide.setResizable(false);
 		frmServerSide.setTitle("Server Side - Listening To Phone");
-		frmServerSide.setBounds(100, 100, 450, 300);
+		frmServerSide.setBounds(100, 100, 443, 300);
 		frmServerSide.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmServerSide.getContentPane().setLayout(null);
+		
+		rbFloat = new JRadioButton("Direct float");
+		rbFloat.setEnabled(false);
+		rbFloat.setBounds(158, 7, 109, 23);
+		frmServerSide.getContentPane().add(rbFloat);
+		
+		JRadioButton rbObject = new JRadioButton("Object");
+		rbObject.setSelected(true);
+		rbObject.setBounds(158, 36, 109, 23);
+		frmServerSide.getContentPane().add(rbObject);
+		
+		rg = new ButtonGroup();
+		rg.add(rbFloat);
+		rg.add(rbObject);
 		
 		JLabel lblPort = new JLabel("Port:");
 		lblPort.setBounds(10, 11, 37, 14);
 		frmServerSide.getContentPane().add(lblPort);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 155, 414, 95);
+		scrollPane.setBounds(10, 155, 207, 95);
 		frmServerSide.getContentPane().add(scrollPane);
 		
-		JTextArea taLog = new JTextArea();
-		scrollPane.setViewportView(taLog);
-		taLog.setWrapStyleWord(true);
-		taLog.setLineWrap(true);
+		taControlLog = new JTextArea();
+		taControlLog.setEditable(false);
+		scrollPane.setViewportView(taControlLog);
+		taControlLog.setWrapStyleWord(true);
+		taControlLog.setLineWrap(true);
 		
-		JButton btnConnectAndSend = new JButton("Listen");
+		btnConnectAndSend = new JButton("Listen");
+		btnConnectAndSend.addActionListener(this);
 		
-		btnConnectAndSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Server server = new Server(btnConnectAndSend, taLog.getDocument());
-				try {
-					server.setPort(Integer.parseInt(tfPort.getText().toString())); 
-				
-				} catch (NumberFormatException exception) {
-				
-					server.setPort(700);
-					System.out.println("No port provided, set to default: 700");
-					
-				}
-				btnConnectAndSend.setEnabled(false);
-				server.execute();
-			}
-			}
-		);
-		
-		btnConnectAndSend.setBounds(10, 36, 165, 23);
+		btnConnectAndSend.setBounds(10, 36, 122, 23);
 		frmServerSide.getContentPane().add(btnConnectAndSend);
 		
 		tfPort = new JTextField();
-		tfPort.setBounds(43, 8, 86, 20);
+		tfPort.setBounds(43, 8, 89, 20);
 		frmServerSide.getContentPane().add(tfPort);
 		tfPort.setColumns(10);
 		
@@ -99,14 +109,46 @@ public class GUI {
 		lblIP.setBounds(10, 76, 67, 14);
 		frmServerSide.getContentPane().add(lblIP);
 		
-		/*
-		JLabel lbIPValue = new JLabel(server.getIP());
-		lbIPValue.setBounds(83, 76, 137, 14);
-		frmServerSide.getContentPane().add(lbIPValue);
-		*/
-		JLabel lblLog = new JLabel("Log:");
-		lblLog.setBounds(10, 140, 46, 14);
-		frmServerSide.getContentPane().add(lblLog);
+		JLabel lblControlLog = new JLabel("Control Log:");
+		lblControlLog.setBounds(10, 140, 86, 14);
+		frmServerSide.getContentPane().add(lblControlLog);
 		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(233, 156, 194, 95);
+		frmServerSide.getContentPane().add(scrollPane_1);
+		
+		taDataLog = new JTextArea();
+		taDataLog.setEditable(false);
+		scrollPane_1.setViewportView(taDataLog);
+		taDataLog.setWrapStyleWord(true);
+		taDataLog.setLineWrap(true);
+		
+		JLabel lbDataLog = new JLabel("Data Log:");
+		lbDataLog.setBounds(233, 140, 67, 14);
+		frmServerSide.getContentPane().add(lbDataLog);
+		
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == btnConnectAndSend) {
+			String mode = (rg.getSelection() == rbFloat.getModel())? "float" : "object" ;
+			Server server = new Server(btnConnectAndSend, taControlLog.getDocument(), taDataLog.getDocument(), mode);
+			try {
+				server.setPort(Integer.parseInt(tfPort.getText().toString())); 
+			} catch (NumberFormatException exception) {
+				server.setPort(700);
+				try {
+					taControlLog.getDocument().insertString(taControlLog.getDocument().getLength(), "No port provided, set to default: 700\n", null);
+				} catch (BadLocationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				//System.out.println("No port provided, set to default: 700");
+			}
+		btnConnectAndSend.setEnabled(false);
+		server.execute();
+		}
 	}
 }
--- TalkWithPhoneViaWifi/src/serverSide/Server.java
+++ TalkWithPhoneViaWifi/src/serverSide/Server.java
@@ -2,8 +2,11 @@ package serverSide;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,18 +20,25 @@ import javax.swing.SwingWorker;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
+import com.example.noamm_000.talkwithcompviawifi.CommonServerAndClient.DataObject;
+
 public class Server extends SwingWorker<Void, String> {
 
 	private int port;
+	private String mode;
 	//private JTextArea taLog;
-	private Document taDoc;
+	private Document taControlDoc;
+	private Document taDataDoc;
 	private JButton restartBtn;
 	
-	public Server(JButton btn, Document doc) {
+	public Server(JButton btn, Document cDoc, Document dDoc, String mode) {
 		restartBtn = btn;
-		taDoc = doc;
+		taControlDoc = cDoc;
+		taDataDoc = dDoc;
+		this.mode = mode;
 		try {
-			taDoc.remove(0, taDoc.getLength());
+			taControlDoc.remove(0, taControlDoc.getLength());
+			taDataDoc.remove(0, taDataDoc.getLength());
 		} catch (BadLocationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -67,17 +77,60 @@ public class Server extends SwingWorker<Void, String> {
 			publish("Client connected!\n Bytes recived:");
 			InputStream is = null;
 			is = socket.getInputStream();
+			ObjectInputStream objectInputStream = new ObjectInputStream(is);
 			int x = 700;
 			int y = 500;
 			byte[] floatInByteArr = new byte[4];
 			int bytesRecived = 0; 
-			while ((is.read(floatInByteArr)) == 4) {	
-				float value = ByteBuffer.wrap(floatInByteArr).order(ByteOrder.BIG_ENDIAN).getFloat();
-				y += value;
-				robot.mouseMove(x, y);		
-				bytesRecived += 4;
-				publish(Integer.toString(bytesRecived));
+			if (mode.equals("float")) {
+				while ((is.read(floatInByteArr)) == 4) {	
+					float value = ByteBuffer.wrap(floatInByteArr).order(ByteOrder.BIG_ENDIAN).getFloat();
+					bytesRecived += 4;
+					publish(Integer.toString(bytesRecived), Float.toString(value));
+					//publish("D", Float.toString(value));
+					if(value == 1) {
+						int mask = InputEvent.BUTTON1_DOWN_MASK;
+						robot.mousePress(mask);
+						robot.mouseRelease(mask);
+					}
+					else if(value == 2) {
+						int mask = InputEvent.BUTTON3_DOWN_MASK;
+						robot.mousePress(mask);
+						robot.mouseRelease(mask);
+					}
+					else {
+						y += value;
+						//publish("D", Float.toString(value));
+						robot.mouseMove(x, y);			
+					}
 				}
+				server.close();
+			}
+			else if(mode.equals("object")) {
+				DataObject dataObject;
+				Object obj;
+				while((obj = objectInputStream.readObject()) != null) {
+					dataObject = (DataObject) obj;
+					y += dataObject.getyDelta();
+					x += dataObject.getxDelta();
+					robot.mouseMove(x, y);
+					if(dataObject.isClickedUp()) {
+						robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+						robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+					}
+					else if(dataObject.isClickedDown()) {
+						robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+						robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+					}
+					bytesRecived += 4;
+					publish(Integer.toString(bytesRecived), "New position is: (" + Integer.toString(x) + " , " + Integer.toString(y) + ")\n");
+					
+				}
+			
+			}
+			//server.close();
+		} catch(EOFException e) {
+			publish("\nNo more objects from client");
 			server.close();
 		} catch (SocketTimeoutException e) {
 			if(null != server) server.close();
@@ -91,15 +144,27 @@ public class Server extends SwingWorker<Void, String> {
 		} 
 		return null;
 	}
+		
 
 	@Override
 	protected void process(List<String> chunks) {
 		// TODO Auto-generated method stub
 		super.process(chunks);
-		for(String publishedData : chunks) {
-			//Document doc = tpLog.getDocument();
+		if(chunks.size() == 1) {
+			String ctrlMsg = chunks.get(0);
 			try {
-				taDoc.insertString(taDoc.getLength(), publishedData + " " , null);
+				taControlDoc.insertString(taControlDoc.getLength(), ctrlMsg + " " , null);
+			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if(chunks.size() > 1) {
+			String ctrlMsg = chunks.get(0);
+			String dataMsg = chunks.get(1);
+			try {
+				taControlDoc.insertString(taControlDoc.getLength(), ctrlMsg + " " , null);
+				taDataDoc.insertString(taDataDoc.getLength(), dataMsg + " " , null);
 			} catch (BadLocationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
